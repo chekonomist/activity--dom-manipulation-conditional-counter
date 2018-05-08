6f3ca3ce1f08e2192e2419a7b230fab4441f9cc1
chekonomist <don.sergio.guerrero@gmail.com>
2018-05-07 21:56:30 -0500

    example done

--- js/app.js
+++ js/app.js
@@ -2,22 +2,44 @@ document
   .querySelector('.btn-increment')
   .addEventListener('click', function increaseCount(evt){
     // YOUR CODE for INCREMENT BTN HERE
+    var totalCountsEl = document.querySelector('.total-counts')
+    var clickCountNumStr = totalCountsEl.textContent
+    var clickCountNum = parseInt(clickCountNumStr)
 
+    if ( totalCountsEl.textContent < 11){
+      totalCountsEl.textContent = clickCountNum + 1
+    }
+
+    if ( totalCountsEl.textContent >= 10) {
+      totalCountsEl.textContent = 10
+      alert('Max number is 10')
+    }
 
   })
 
 
 document
-  .querySelector('.btn-subtract')
+  .querySelector('.btn-decrement')
   .addEventListener('click', function decreaseCount(evt){
     // YOUR CODE for DECREMENT BTN HERE
+    var totalCountsEl = document.querySelector('.total-counts')
+    var clickCountNumStr = totalCountsEl.textContent
+    var clickCountNum = parseInt(clickCountNumStr)
 
+    if ( totalCountsEl.textContent < 11){
+      totalCountsEl.textContent = clickCountNum - 1
+    }
+
+    if (totalCountsEl.textContent <= 0) {
+      totalCountsEl.textContent = 0
+      alert('Minimum number is 0')
+    }
   })
 
 
 /* ---- ADVENTURE MODE ---- */
 /* Use only one Event Listener on the parent
-    <div class="component"> 
+    <div class="component">
  */
 
 /* Uncommment....*/
